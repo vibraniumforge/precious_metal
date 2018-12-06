4227d77a716b732387c2bbbbeb50c5a3c66b0932
Greg Morgan <gregm9999@gmail.com>
2018-12-06 03:02:09 +0000

    v6

--- lib/precious_metal/cli.rb
+++ lib/precious_metal/cli.rb
@@ -24,7 +24,7 @@ class PreciousMetal::CLI
             user_input=gets.strip
             if user_input.to_i.between?(1,4)
                 the_choice=PreciousMetal::Metal.all[user_input.to_i-1]
-                puts "The price of #{the_choice.name} is #{the_choice.price} U.S. dollars per Troy Ounce."           
+                puts "The current price of #{the_choice.name} is #{the_choice.price} U.S. dollars per Troy Ounce."           
             elsif user_input =="list"
               puts "Welcome to the precious metal price app."
                puts "Please choose one of the four precious metals to see their current price."
